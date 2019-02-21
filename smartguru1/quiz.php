503080562c76a7481ce6afe57a962fd5dcf8ce3e
sanethinduwara <47098118+sanethinduwara@users.noreply.github.com>
2019-02-21 23:45:55 +0530

    Add files via upload

--- smartguru1/quiz.php
+++ smartguru1/quiz.php
@@ -1,136 +1,98 @@
 <!doctype html>
 <html>
+
 <head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
+	<meta charset="utf-8">
+	<title>Untitled Document</title>
 </head>
 
 <body>
 
 
-<span id="countdown"></span>
-	
-	
-<script>
-/*var seconds = 40;
-    function secondPassed() {
-    var minutes = Math.round((seconds - 30)/60);
-    var remainingSeconds = seconds % 60;
-    if (remainingSeconds < 10) {
-        remainingSeconds = "0" + remainingSeconds; 
-    }
-    document.getElementById('countdown').innerHTML = minutes + ":" +    remainingSeconds;
-    if (seconds == 0) {
-        clearInterval(countdownTimer);
-		document.getElementById("myForm").submit();
-        document.getElementById('countdown').innerHTML = "Time Over ";
-    } else {    
-        seconds--;
-    }
-    }
-var countdownTimer = setInterval('secondPassed()', 1000);
-	
-	*/
-	var seconds = 0, minutes = 0, hours = 0,
-    t;
-	
-	function add() {
-    seconds++;
-    if (seconds >= 60) {
-        seconds = 0;
-        minutes++;
-        if (minutes >= 60) {
-            minutes = 0;
-            hours++;
-        }
-    }
-    
-    //h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
-		
-		document.getElementById('timer1').value = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
-		//document.getElementById("timer1").readOnly = true;
-		
+	<span id="countdown"></span>
 
-    timer();
-}
-function timer() {
-    t = setTimeout(add, 1000);
-}
-timer();
-	
-	
-</script>
 
-<?php
-include_once 'dbConnection.php';
-	 
-if(@$_GET['q']== 'quiz'){
-$lesson_id=@$_GET['id'];
-$seq_no=@$_GET['n'];
-$total=@$_GET['t'];
-	
-	
-	//display the question
-$query=mysqli_query($con,"SELECT * FROM question WHERE lesson_id='$lesson_id' AND seq_no='$seq_no' " );
-echo '<div class="panel" style="margin:5%">';
-while($row=mysqli_fetch_array($query) )
-{
-$question=$row['question_desc'];
-$qs_id=$row['question_id'];
-echo '<b>Question &nbsp;'.$seq_no.'&nbsp;:<br />'.$question.'</b><br /><br />';
-}
-	
-	//display options for the question
-$query=mysqli_query($con,"SELECT * FROM options WHERE question_id='$qs_id' " );
-echo '<form id="myForm" action="update.php?q=quiz&id='.$lesson_id.'&n='.$seq_no.'&t='.$total.'&qs_id='.$qs_id.'" method="POST"  class="form-horizontal">
+	<script>
+
+		var seconds = 0,
+			minutes = 0,
+			hours = 0,
+			t;
+
+		function add() {
+			seconds++;
+			if ( seconds >= 60 ) {
+				seconds = 0;
+				minutes++;
+				if ( minutes >= 60 ) {
+					minutes = 0;
+					hours++;
+				}
+			}
+
+			document.getElementById( 'timer1' ).value = ( hours ? ( hours > 9 ? hours : "0" + hours ) : "00" ) + ":" + ( minutes ? ( minutes > 9 ? minutes : "0" + minutes ) : "00" ) + ":" + ( seconds > 9 ? seconds : "0" + seconds );
+			document.getElementById("timer1").readOnly = true;
+
+
+			timer();
+		}
+
+		function timer() {
+			t = setTimeout( add, 1000 );
+		}
+		timer();
+	</script>
+
+	<?php
+	session_start();
+	include_once 'dbConnection.php';
+
+	if ( @$_GET[ 'q' ] == 'quiz' ) {
+		$lesson_id = @$_GET[ 'id' ];
+		$seq_no = @$_GET[ 'n' ];
+		$total = @$_GET[ 't' ];
+		$score = @$_GET[ 'score' ];
+
+
+
+		//display the question
+		$query = mysqli_query( $con, "SELECT * FROM question WHERE lesson_id='$lesson_id' AND seq_no='$seq_no' " );
+		echo '<div class="panel" style="margin:5%">';
+		while ( $row = mysqli_fetch_array( $query ) ) {
+			$question = $row[ 'question_desc' ];
+			$qs_id = $row[ 'question_id' ];
+
+			echo '<b>Question &nbsp;' . $seq_no . '&nbsp;:<br />' . $question . '</b><br /><br />';
+		}
+
+		//display options for the question
+		$query = mysqli_query( $con, "SELECT * FROM options WHERE question_id='$qs_id' " );
+		echo '<form id="myForm" action="update.php?q=quiz&id=' . $lesson_id . '&n=' . $seq_no . '&t=' . $total . '&qs_id=' . $qs_id . '&score=' . $score . '" method="POST"  class="form-horizontal">
 <input type="text" id="timer1" name="timer1"><br /><br />';
 
-while($row=mysqli_fetch_array($query) )
-{
-$option=$row['option_text'];
-$optionid=$row['option_id'];
-echo'<input type="radio" name="answer" value="'.$optionid.'" required>'.$option.'<br /><br />';
-}
-echo'<br /><button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-lock" aria-hidden="true"></span>&nbsp;Submit</button></form></div>';
-//header("location:dash.php?q=4&step=2&eid=$id&n=$total");
-}
+		while ( $row = mysqli_fetch_array( $query ) ) {
+			$option = $row[ 'option_text' ];
+			$optionid = $row[ 'option_id' ];
+			echo '<input type="radio" name="answer" value="' . $optionid . '" required>' . $option . '<br /><br />';
+		}
+		echo '<br /><button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-lock" aria-hidden="true"></span>&nbsp;Submit</button></form></div>';
+		
+	}
 
 
+	//result display
+	if ( @$_GET[ 'q' ] == 'result' && @$_GET[ 'lesson_id' ] ) {
+		$lesson_id = @$_GET[ 'lesson_id' ];
+		$score = @$_GET[ 'score' ];
+		$username = $_SESSION[ "email" ];
+		$date = date( "Y-m-d H:i:s" );
+		$query = mysqli_query( $con, "INSERT INTO user_history VALUES ('$username', '$lesson_id', '$score', '$date') " )or die( 'Error' );
+		echo 'Score : ' . $score . '';
 
 
+	}
+	?>
 
+</body>
 
-
-
-//result display
-if(@$_GET['q']== 'result' && @$_GET['eid']) 
-{
-$eid=@$_GET['eid'];
-$q=mysqli_query($con,"SELECT * FROM history WHERE eid='$eid' AND email='$email' " )or die('Error157');
-echo  '<div class="panel">
-<center><h1 class="title" style="color:#660033">Result</h1><center><br /><table class="table table-striped title1" style="font-size:20px;font-weight:1000;">';
-
-while($row=mysqli_fetch_array($q) )
-{
-$s=$row['score'];
-$w=$row['wrong'];
-$r=$row['sahi'];
-$qa=$row['level'];
-echo '<tr style="color:#66CCFF"><td>Total Questions</td><td>'.$qa.'</td></tr>
-      <tr style="color:#99cc32"><td>right Answer&nbsp;<span class="glyphicon glyphicon-ok-circle" aria-hidden="true"></span></td><td>'.$r.'</td></tr> 
-	  <tr style="color:red"><td>Wrong Answer&nbsp;<span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span></td><td>'.$w.'</td></tr>
-	  <tr style="color:#66CCFF"><td>Score&nbsp;<span class="glyphicon glyphicon-star" aria-hidden="true"></span></td><td>'.$s.'</td></tr>';
-}
-$q=mysqli_query($con,"SELECT * FROM rank WHERE  email='$email' " )or die('Error157');
-while($row=mysqli_fetch_array($q) )
-{
-$s=$row['score'];
-echo '<tr style="color:#990000"><td>Overall Score&nbsp;<span class="glyphicon glyphicon-stats" aria-hidden="true"></span></td><td>'.$s.'</td></tr>';
-}
-echo '</table></div>';
-
-}
-?>
-	
-	</body>
 </html>
--- smartguru1/update.php
+++ smartguru1/update.php
@@ -15,6 +15,8 @@
 session_start();
 include_once 'dbConnection.php';
 
+$score = 0;
+
 if ( @$_GET[ 'q' ] == 'quiz' ) {
 	$lesson_id = @$_GET[ 'id' ];
 	$sn = @$_GET[ 'n' ];
@@ -24,7 +26,8 @@ if ( @$_GET[ 'q' ] == 'quiz' ) {
 	$qs_id = @$_GET[ 'qs_id' ];
 	$username = $_SESSION[ "email" ];
 	$is_correct = 0;
-	
+	$score = @$_GET[ 'score' ];
+
 
 	$query = mysqli_query( $con, "SELECT * FROM answer WHERE question_id='$qs_id' " );
 	while ( $row = mysqli_fetch_array( $query ) ) {
@@ -34,64 +37,27 @@ if ( @$_GET[ 'q' ] == 'quiz' ) {
 	//if the given anser is correct
 	if ( $answer == $answer_id ) {
 		$is_correct = 1;
-				
-		$query = mysqli_query( $con, "SELECT * FROM lesson WHERE lesson_id='$lesson_id' " );
+
+		$query = mysqli_query( $con, "SELECT * FROM question WHERE question_id='$qs_id' " );
 		while ( $row = mysqli_fetch_array( $query ) ) {
-			//$sahi=$row['sahi'];
+			$score += $row[ 'points' ];
 		}
 		if ( $sn == 1 ) {
-			//$query=mysqli_query($con,"INSERT INTO history VALUES('$email','$eid' ,'0','0','0','0',NOW())")or die('Error');
-		}
-		//$query=mysqli_query($con,"SELECT * FROM history WHERE eid='$eid' AND email='$email' ")or die('Error115');
 
-		//while($row=mysqli_fetch_array($q) )
-		{
-			//$s=$row['score'];
-			//$r=$row['sahi'];
 		}
-		//$r++;
-		//$s=$s+$sahi;
-		//$q=mysqli_query($con,"UPDATE `history` SET `score`=$s,`level`=$sn,`sahi`=$r, date= NOW()  WHERE  email = '$email' AND eid = '$eid'")or die('Error124');
 
 	} else {
-		
+
 	}
 	$query = mysqli_query( $con, "INSERT INTO user_answers VALUES ('$username','$qs_id', '$is_correct', '$time') " );
 
-	//if the given answer is incorrect
-	/*	
-	else
-	{
-	$q=mysqli_query($con,"SELECT * FROM lesson WHERE eid='$lesson_id' " )or die('Error129');
-
-	while($row=mysqli_fetch_array($q) )
-	{
-	$wrong=$row['wrong'];
-	}
-	if($sn == 1)
-	{
-	$q=mysqli_query($con,"INSERT INTO history VALUES('$email','$lesson_id' ,'0','0','0','0',NOW() )")or die('Error137');
-	}
-	$q=mysqli_query($con,"SELECT * FROM history WHERE eid='$lesson_id' AND email='$email' " )or die('Error139');
-	while($row=mysqli_fetch_array($q) )
-	{
-	$s=$row['score'];
-	$w=$row['wrong'];
-	}
-	$w++;
-	$s=$s-$wrong;
-	$q=mysqli_query($con,"UPDATE `history` SET `score`=$s,`level`=$sn,`wrong`=$w, date=NOW() WHERE  email = '$email' AND eid = '$lesson_id'")or die('Error147');
-	}
-		*/
-
-	
 
 	if ( $sn != $total ) {
 		$sn++;
-		header( "location:quiz.php?q=quiz&id=$lesson_id&n=$sn&t=$total" )or die( 'Error152' );
+		header( "location:quiz.php?q=quiz&id=$lesson_id&n=$sn&t=$total&score=$score" )or die( 'Error152' );
 	}
 
-	//header("location:account.php?q=result&eid=$eid");
+	header( "location:quiz.php?q=result&lesson_id=$lesson_id&score=$score" );
 
 }
 
